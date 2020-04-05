34da011f2bfd10d22013fccd37a72ccb26536d9d
Loop <61188554+Loop1337@users.noreply.github.com>
2020-04-05 02:40:10 +0100

    Add files via upload

---
+++ storage/framework/views/7f08043e5a033fd08725e98d147dc3eb6b3f7aa9.php
@@ -0,0 +1,62 @@
+<?php $__env->startSection('content'); ?>
+<div class="container">
+    <div class="row">
+
+        <div class="col-3 p-5">
+            <img src="/freecodecamplogo/logo.jpg" class="rounded-circle">
+        </div>
+        <div class="col-9 pt-5" >
+            <div>
+                <h1>
+                    freeCodeCamp
+                </h1>
+            </div>
+                <div class="d-flex">
+                    <div class="pr-5">
+                        <strong>153</strong>posts
+                    </div>
+                    <div class="pr-5">
+                        <strong>23k</strong>followers
+            </div>
+            <div class="pr-5">
+                <strong>212</strong>following
+            </div>
+            </div>
+            <div class="pt-4 font-weight-bold">
+                freeCodeCamp.org
+            </div>
+            <div>
+                We're a global community of millions of people learning to code together. We're an open source, donor-supported, 501(c)(3) nonprofit.
+            </div>
+            <div>
+                <a href="#">www.freecodecamp.org</a>
+            </div>
+
+        </div>
+    </div>
+    <div class="row pt-5">
+        <div class="col-4">
+            <img src="/freecodecamplogo/4.jpg" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="/freecodecamplogo/2.jpg" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="/freecodecamplogo/3.jpg" class="w-100">
+        </div>
+    </div>
+    <div class="row pt-4">
+        <div class="col-4">
+            <img src="/freecodecamplogo/2.jpg" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="/freecodecamplogo/3.jpg" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="/freecodecamplogo/4.jpg" class="w-100">
+        </div>
+    </div>
+</div>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\ABYDONY\FreeCodeGram\resources\views/home.blade.php ENDPATH**/ ?>
